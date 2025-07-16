81a5716ca2c5612fef595948f0adc66711aeeea4
samG0406 <136176940+sambhaviG@users.noreply.github.com>
2025-07-16 14:45:49 -0400

    Complete RideTogether app setup with authentication, styling, and API routes

---
+++ app/(root)/(tabs)/home.tsx
@@ -0,0 +1,16 @@
+import { SignedIn, useUser } from "@clerk/clerk-expo";
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+// import { SignOutButton } from "@/components/signOutButton";
+
+export default function Page() {
+  const { user } = useUser();
+
+  return (
+    <SafeAreaView>
+      <SignedIn>
+        <Text>Hello {user?.emailAddresses[0].emailAddress}</Text>
+      </SignedIn>
+    </SafeAreaView>
+  );
+}
